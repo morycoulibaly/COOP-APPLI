a46cbe7ed5380a2a8c4b8ef7325f3efea2468bf4
tresor papillon <mory.coulibaly@epitech.eu>
2026-08-02 04:32:38 +0000

    Ajout du module Cotisation dans le app.module

--- backend/coop_appli/src/app.module.ts
+++ backend/coop_appli/src/app.module.ts
@@ -4,14 +4,18 @@ import { UsersModule } from './users/users.module';
 import { AuthModule } from './auth/auth.module';
 import { AdminModule } from './admin/admin.module';
 import { EvenementModule } from './evenement/evenement.module';
+import { CotisationModule } from './cotisation/cotisation.module';
+import { JournalModule } from './journal/journal.module';
 
 @Module({
   imports: [
     PrismaModule,
+    JournalModule,
     UsersModule,
     AuthModule,
     AdminModule,
     EvenementModule,
+    CotisationModule,
   ],
 })
 export class AppModule {}
